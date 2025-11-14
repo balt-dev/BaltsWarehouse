6d4f0b5a9dc1dae5e8949320924cd632115738f2
balt-dev <heptor42@gmail.com>
2025-11-14 14:48:26 -0600

    Fix exploding with Talisman

--- src/lib/decimal_ante.lua
+++ src/lib/decimal_ante.lua
@@ -1,6 +1,7 @@
 local orig_get_blind_amount = get_blind_amount
 
 function get_blind_amount(ante)
+	if type(ante) == "table" then return orig_get_blind_amount(math.floor(ante)) end
 	local fract_ante = math.fmod(ante, 1.0)
 	if fract_ante == 0 or fract_ante ~= fract_ante or not (fract_ante > -math.huge and fract_ante < math.huge) then
 		return orig_get_blind_amount(ante)
